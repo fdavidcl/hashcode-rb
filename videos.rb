bef79891b511b325e1baad6c56da8a9844e87e4d
David Charte <fdavidcl@protonmail.com>
2017-02-23 19:39:22 +0100

    Ganancias

--- videos.rb
+++ videos.rb
@@ -29,8 +29,21 @@ dc_latencies, endpoints = (0 ... num_end).map do |i|
   { dc_lat: dl, caches: caches }
 end.map(&:each_value).map(&:to_a).transpose
 
+gain = (0 ... endpoints.length).map do |i|
+  endpoints[i].map { |e| e - dc_latencies[i] }
+end
+
 requests = (0 ... num_req).map do |i|
   video_id, endpoint_id, reqs = lines.shift.split.map &:to_i
   { video: video_id, endpoint: endpoint_id, reqs: reqs }
 end
 
+count_caches = endpoints.transpose.map { |e| e.reduce(0) { |ac, c| ac + (c.nil? ? 0 : 1) } }
+  
+def backpack_heuristic
+  cache_capacity = Array.new(num_cache, capacity)
+  
+  cache_capacity.each do |cap|
+    
+  end
+end
